b690b1b193aaf1ad01a2b908ddc0c1303fe5a061
Miłosz Skaza <milosz.skaza@ctfd.io>
2022-11-14 18:43:17 +0100

    fix paths and config (#6)
    * improve static dir resolution 
    * import metadata for openapi from package.json
    * add config option for static directory

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,12 +1,18 @@
+import * as fs from "fs";
+import * as path from "path";
+
 import { FastifyRegisterOptions } from "fastify";
 import { FastifyDynamicSwaggerOptions } from "@fastify/swagger";
+
+const PkgPath = path.normalize(path.join(__dirname, "..", "package.json"));
+const PKG = JSON.parse(fs.readFileSync(PkgPath).toString());
 
 export const SwaggerConfig: FastifyRegisterOptions<FastifyDynamicSwaggerOptions> = {
   openapi: {
     info: {
-      title: "CTFd Tourist",
-      description: "testing the fastify swagger api",
-      version: "0.1.0",
+      title: PKG.name,
+      description: PKG.description,
+      version: PKG.version,
     },
     servers: [
       {
