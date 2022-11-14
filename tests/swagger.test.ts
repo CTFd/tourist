72eb99b42651bda2a104d297c22019e878e0b7b1
Miłosz Skaza <milosz.skaza@ctfd.io>
2022-11-14 18:41:34 +0100

    test app creates openapi

--- tests/swagger.test.ts
+++ tests/swagger.test.ts
@@ -1,5 +1,24 @@
+import * as fs from "fs";
+import * as path from "path";
+
 import test from "ava";
 import { createApp } from "../src/app";
+
+test("createApp creates openapi.json", async t => {
+  const openapi = path.resolve(__dirname, "..", "static", "openapi.json");
+  if (openapi) {
+    fs.rmSync(openapi);
+  }
+
+  const app = await createApp({ logger: false });
+  const response = await app.inject({
+    method: "GET",
+    url: "/openapi.json",
+  });
+
+  t.is(response.statusCode, 200);
+  t.snapshot(response.json());
+});
 
 test("GET '/openapi.json' returns the specification", async t => {
   const app = await createApp({ logger: false });
