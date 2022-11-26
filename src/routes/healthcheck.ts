cc76e77fd00aa552a39015471e202c6562be7c1a
Miłosz Skaza <milosz.skaza@ctfd.io>
2022-11-26 13:05:17 +0100

    simplify handlers (#23)

--- src/routes/healthcheck.ts
+++ src/routes/healthcheck.ts
@@ -10,13 +10,11 @@ export default (
   options: FastifyPluginOptions,
   done: (err?: Error | undefined) => void,
 ) => {
-  fastify.get("/healthcheck", { handler: getHealthcheckHandler(fastify) });
+  fastify.get("/healthcheck", { handler: handleHealthcheck });
 
   done();
 };
 
-const getHealthcheckHandler = (fastify: FastifyInstance) => {
-  return (request: FastifyRequest, reply: FastifyReply) => {
-    reply.send({ status: "OK" });
-  };
+const handleHealthcheck = (request: FastifyRequest, reply: FastifyReply) => {
+  reply.send({ status: "OK" });
 };
--- src/routes/legacy.ts
+++ src/routes/legacy.ts
@@ -36,145 +36,142 @@ export default (
       body: LegacyVisitRequest,
       response: { 200: LegacyVisit200Reply, 400: LegacyVisit400Reply },
     },
-    handler: getVisitHandler(fastify),
+    handler: handleVisit,
   });
 
   done();
 };
 
-const getVisitHandler = (fastify: FastifyInstance) => {
-  return async (request: FastifyRequest, reply: FastifyReply) => {
-    const { steps, cookies, record, screenshot, pdf } = <LegacyVisitRequestType>(
-      request.body
-    );
+const handleVisit = async (request: FastifyRequest, reply: FastifyReply) => {
+  const { steps, cookies, record, screenshot, pdf } = <LegacyVisitRequestType>(
+    request.body
+  );
 
-    if ([record, screenshot, pdf].filter(Boolean).length > 1) {
-      reply.status(400).send({
-        statusCode: 400,
-        error: "Bad Request",
-        message:
-          "Exactly one option of [record, screenshot, pdf] can be used at a time",
-      });
+  if ([record, screenshot, pdf].filter(Boolean).length > 1) {
+    reply.status(400).send({
+      statusCode: 400,
+      error: "Bad Request",
+      message: "Exactly one option of [record, screenshot, pdf] can be used at a time",
+    });
+  }
+
+  for (const step of steps) {
+    if (step.actions) {
+      try {
+        validateLegacyActions(step.actions);
+      } catch (e: any) {
+        request.log.error(e.message);
+
+        return reply.status(400).send({
+          statusCode: 400,
+          error: "Bad Request",
+          message: e.message,
+        });
+      }
+    }
+  }
+
+  for (const step of steps) {
+    if (step.actions) {
+      step.actions = camelizeLegacyActions(step.actions);
+    }
+  }
+
+  const browser = await playwright.chromium.launch();
+
+  let context;
+  if (record) {
+    context = await browser.newContext({ recordVideo: { dir: os.tmpdir() } });
+  } else {
+    context = await browser.newContext();
+  }
+
+  if (cookies) {
+    await context.addCookies(cookies);
+  }
+
+  const page = await context.newPage();
+
+  for (const step of steps) {
+    const actions = {
+      preOpen: [] as string[],
+      postOpen: [] as string[],
+    };
+
+    // split step actions into preOpen and postOpen
+    // listeners such as page.on need to be registered before page.goto()
+    if (step.actions) {
+      [actions.preOpen, actions.postOpen] = _.partition(step.actions, action =>
+        action.startsWith("page.on"),
+      );
     }
 
-    for (const step of steps) {
-      if (step.actions) {
-        try {
-          validateLegacyActions(step.actions);
-        } catch (e: any) {
-          fastify.log.error(e.message);
+    const vm = new VM({
+      eval: false,
+      wasm: false,
+      sandbox: {
+        page,
+      },
+    });
 
-          return reply.status(400).send({
-            statusCode: 400,
-            error: "Bad Request",
-            message: e.message,
-          });
-        }
+    for (const preOpenAction of actions.preOpen) {
+      try {
+        await (async () => vm.run(preOpenAction))();
+      } catch (e) {
+        request.log.error(e);
+        return reply.status(400).send({
+          statusCode: 400,
+          error: "Bad Request",
+          message: `invalid action "${preOpenAction}"`,
+        });
       }
     }
 
-    for (const step of steps) {
-      if (step.actions) {
-        step.actions = camelizeLegacyActions(step.actions);
+    await page.goto(step.url);
+
+    for (const postOpenAction of actions.postOpen) {
+      try {
+        await (async () => vm.run(postOpenAction))();
+        await page.waitForLoadState();
+      } catch (e) {
+        request.log.error(e);
+        return reply.status(400).send({
+          statusCode: 400,
+          error: "Bad Request",
+          message: `invalid action "${postOpenAction}"`,
+        });
       }
     }
+  }
 
-    const browser = await playwright.chromium.launch();
+  if (screenshot) {
+    const screenshotBuffer = await page.screenshot({ fullPage: true });
+    const file = screenshotBuffer.toString("base64");
+    return reply.send({ status: "ok", screenshot: file });
+  }
 
-    let context;
-    if (record) {
-      context = await browser.newContext({ recordVideo: { dir: os.tmpdir() } });
-    } else {
-      context = await browser.newContext();
+  if (pdf) {
+    const pdfBuffer = await page.pdf();
+    const file = pdfBuffer.toString("base64");
+    return reply.send({ status: "ok", pdf: file });
+  }
+
+  // video is only accessible after context is closed, but required open for screenshot / pdf
+  await context.close();
+  await browser.close();
+
+  if (record) {
+    const video = await page.video();
+
+    if (video) {
+      const path = await video.path();
+      const videoBuffer = fs.readFileSync(path);
+      const file = videoBuffer.toString("base64");
+      fs.unlinkSync(path);
+
+      return reply.send({ status: "ok", video: file });
     }
+  }
 
-    if (cookies) {
-      await context.addCookies(cookies);
-    }
-
-    const page = await context.newPage();
-
-    for (const step of steps) {
-      const actions = {
-        preOpen: [] as string[],
-        postOpen: [] as string[],
-      };
-
-      // split step actions into preOpen and postOpen
-      // listeners such as page.on need to be registered before page.goto()
-      if (step.actions) {
-        [actions.preOpen, actions.postOpen] = _.partition(step.actions, action =>
-          action.startsWith("page.on"),
-        );
-      }
-
-      const vm = new VM({
-        eval: false,
-        wasm: false,
-        sandbox: {
-          page,
-        },
-      });
-
-      for (const preOpenAction of actions.preOpen) {
-        try {
-          await (async () => vm.run(preOpenAction))();
-        } catch (e) {
-          fastify.log.error(e);
-          return reply.status(400).send({
-            statusCode: 400,
-            error: "Bad Request",
-            message: `invalid action "${preOpenAction}"`,
-          });
-        }
-      }
-
-      await page.goto(step.url);
-
-      for (const postOpenAction of actions.postOpen) {
-        try {
-          await (async () => vm.run(postOpenAction))();
-          await page.waitForLoadState();
-        } catch (e) {
-          fastify.log.error(e);
-          return reply.status(400).send({
-            statusCode: 400,
-            error: "Bad Request",
-            message: `invalid action "${postOpenAction}"`,
-          });
-        }
-      }
-    }
-
-    if (screenshot) {
-      const screenshotBuffer = await page.screenshot({ fullPage: true });
-      const file = screenshotBuffer.toString("base64");
-      return reply.send({ status: "ok", screenshot: file });
-    }
-
-    if (pdf) {
-      const pdfBuffer = await page.pdf();
-      const file = pdfBuffer.toString("base64");
-      return reply.send({ status: "ok", pdf: file });
-    }
-
-    // video is only accessible after context is closed, but required open for screenshot / pdf
-    await context.close();
-    await browser.close();
-
-    if (record) {
-      const video = await page.video();
-
-      if (video) {
-        const path = await video.path();
-        const videoBuffer = fs.readFileSync(path);
-        const file = videoBuffer.toString("base64");
-        fs.unlinkSync(path);
-
-        return reply.send({ status: "ok", video: file });
-      }
-    }
-
-    return reply.send({ status: "ok" });
-  };
+  return reply.send({ status: "ok" });
 };
