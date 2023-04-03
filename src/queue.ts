14e8f36c7afee4db33a51c3c82b2d0c760c43349
Miłosz Skaza <milosz.skaza@ctfd.io>
2023-04-03 15:29:39 +0200

    add concurrency setting to config

--- src/queue.ts
+++ src/queue.ts
@@ -2,6 +2,8 @@ import Queue from "bull";
 import { legacySimpleVisitJob, LegacySimpleVisitJobData } from "./jobs/legacy";
 import { asyncVisitJob, VisitJobData } from "./jobs/api";
 
+import config from "./config";
+
 const LegacySimpleVisitQueue = new Queue<LegacySimpleVisitJobData>(
   "Legacy Simple Visit Job",
   process.env.REDIS_URL || "redis://127.0.0.1:6379",
@@ -13,7 +15,7 @@ const AsyncVisitQueue = new Queue<VisitJobData>(
 );
 
 // Assign worker functions to queues
-LegacySimpleVisitQueue.process(legacySimpleVisitJob);
-AsyncVisitQueue.process(asyncVisitJob);
+LegacySimpleVisitQueue.process(config.CONCURRENCY, legacySimpleVisitJob);
+AsyncVisitQueue.process(config.CONCURRENCY, asyncVisitJob);
 
 export { LegacySimpleVisitQueue, AsyncVisitQueue };
