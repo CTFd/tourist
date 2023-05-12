7ffeba9a864d3dc50625c3008c8aced3c16311a0
Miłosz Skaza <milosz.skaza@ctfd.io>
2023-05-12 13:40:46 +0200

    yarn install && yarn format

--- src/loaders/cors.ts
+++ src/loaders/cors.ts
@@ -1,5 +1,5 @@
 import { FastifyInstance } from "fastify";
-import cors from "@fastify/cors"
+import cors from "@fastify/cors";
 
 export const initCORS = (app: FastifyInstance) => {
   if (app.config.ENABLE_CORS) {
